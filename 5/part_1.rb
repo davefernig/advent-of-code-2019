96506dc92633fcecc2fcac782fab161d09ed2289
david.fernig@shopify.com <david.fernig@shopify.com>
2019-12-05 12:08:33 +0100

    day-5-complete

---
+++ 5/part_1.rb
@@ -0,0 +1,50 @@
+A = File
+  .read('input.txt')
+  .strip()
+  .split(",")
+  .collect {|x| x.to_i}
+
+def parse_opcode n
+  opcode = n.to_s.rjust(5).gsub(' ', '0')
+  [
+    opcode[-2..-1].to_i,
+    opcode[-3].to_i,
+    opcode[-4].to_i,
+    opcode[-5].to_i,
+  ]
+end
+
+index = 0
+while A[index] != 99
+  instructions = parse_opcode A[index]
+
+  if instructions[0] == 1
+    p1 = instructions[1] == 0 ? A[A[index+1]] : A[index+1]
+    p2 = instructions[2] == 0 ? A[A[index+2]] : A[index+2]
+    p3 = instructions[3] == 0 ? A[index+3] : index+3
+    A[p3] = p1 + p2
+    index += 4
+  end
+
+  if instructions[0] == 2
+    p1 = instructions[1] == 0 ? A[A[index+1]] : A[index+1]
+    p2 = instructions[2] == 0 ? A[A[index+2]] : A[index+2]
+    p3 = instructions[3] == 0 ? A[index+3] : index+3
+    A[p3] = p1 * p2
+    index += 4
+  end
+
+  if instructions[0] == 3
+    p1 = instructions[1] == 0 ? A[index+1] : index+1
+    input = 1
+    puts "Setting input to: " + input.to_s
+    A[A[index+1]] = input
+    index += 2
+  end
+
+  if instructions[0] == 4
+    p1 = instructions[1] == 0 ? A[index+1] : index+1
+    puts A[A[index+1]]
+    index += 2
+  end
+end
